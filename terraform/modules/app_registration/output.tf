1547a4dbeb340f5abecbaaad3b270ade72b23b61
matthew.ricketts <matthew.ricketts@kainos.com>
2023-11-07 10:47:25 +0000

    Fix formatting

--- terraform/modules/app_registration/output.tf
+++ terraform/modules/app_registration/output.tf
@@ -8,6 +8,6 @@ output "client_secret" {
 }
 
 output "tenant_id" {
-  value = data.azuread_client_config.current.tenant_id
+  value     = data.azuread_client_config.current.tenant_id
   sensitive = true
 }
